4a007f0252e29f3683d0728971e3a3e542fa0c8f
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-12 12:01:55 -0600

    shellpractice

---
+++ 01shell_script.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+
+echo "Pavan: Hi Tarak, How are you"
+echo "Tarak: Hi Pavan, I am good, How are you"
+echo "Pavan: I am good Tarak, Thanks for asking"
+echo "Tarak: How is your day"
+echo "Pavan: Good Tarak"
