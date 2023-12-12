d3c7dc90e3b548c4dea76b4ea8dbcce63c7434d1
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-12 12:49:54 -0600

    varaibles added

--- 01shell_script.sh
+++ 01shell_script.sh
@@ -1,8 +1,10 @@
 #!bin/bash
 
+person1=Pavan
+person2=Tarak
 
-echo "Pavan: Hi Tarak, How are you"
-echo "Tarak: Hi Pavan, I am good, How are you"
-echo "Pavan: I am good Tarak, Thanks for asking"
-echo "Tarak: How is your day"
-echo "Pavan: Good Tarak"
+echo "$person1: Hi $person2, How are you"
+echo "$person2: Hi $person1, I am good, How are you"
+echo "$person1: I am good $person2, Thanks for asking"
+echo "$person2: How is your day $person1"
+echo "$person1: Good $person2"
