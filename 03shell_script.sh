09b3d31b0e61b2953c10cbd5a76f6b55cd752aaf
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-12 13:38:57 -0600

    passing arguments to the variables

---
+++ 03shell_script.sh
@@ -0,0 +1,11 @@
+#!bin bash
+
+Person1=$1
+Person2=$2
+
+echo $Person1: Hi $Person2, Good Morning
+echo $Person2: Hi $Person1, How are you.
+echo $Person1: I am Good $Person2, Thanks for asking.
+echo $Person2: Can we go for a movie tomorrow $Person1.
+echo $Person1: Sure $Person2. Let me know the timing in advance.
+echo $Person2: Ok $Person1
