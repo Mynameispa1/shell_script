ad66fffed98cb792aa1e012b7d777427f15410ab
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-13 16:01:17 -0600

    functions added

---
+++ 06readscript.sh
@@ -0,0 +1,18 @@
+
+
+#!bin/bash
+
+echo "Enter username"
+
+read username
+
+echo "given username is: $username"
+
+echo "Enter password"
+
+read password
+
+echo "entered password is: $password"
+
+
+
--- 10specialvariable.sh
+++ 10specialvariable.sh
@@ -6,3 +6,11 @@ NUMBER2=$2
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "The sum of two numbers is:: $SUM"
+
+echo "How many args passwed is: $#"
+
+echo "All args passwed: $@"
+
+echo "Script name is: $0"
+
+echo "Exit status of previous code of previous code: $?"
