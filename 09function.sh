782564e34ded620c08e9cd61f6f9c0c18e5abcc5
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-13 14:27:40 -0600

    functions added

---
+++ 09function.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+ID=$(id -u)
+function(){
+      if [ $1 -ne 0 ]
+      then
+         echo "Error:: $2 failed"
+         exit 1
+     else
+        echo "$2 successful"
+      fi
+}
+if [ $ID -ne 0 ]
+then
+echo "Error:: Please run script with root user"
+exit 1
+else
+echo "Running with root user"
+fi
+
+yum install mysql -y
+function $? "Installing mysql"
+yum install git -y
+function $? "Installing git"
