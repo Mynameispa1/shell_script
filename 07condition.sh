d1f3dd4760f94feb8dd9c6221537b18deddb6f66
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-13 12:41:04 -0600

    complete script

--- 07condition.sh
+++ 07condition.sh
@@ -1,10 +1,20 @@
 #!bin/bash
 
-if [today!=sunday] {
+<<com
+if [ today!=sunday ] 
+then
     echo "Go to school"
-}
-
-else {
+else 
     echo "Happy Sunday"
-}
+fi
+com
+
+NUMBER=$1
+
+if [ $NUMBER -gt 100 ]
+then
+    echo "The given number $NUMBER is greater then 100"
+
+else
+    echo "The given number $NUMBER is less then 100"
 fi
---
+++ 08install_scripts.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then
+echo "Error:: Please run script with root user"
+else
+echo "Running with root user"
+if
