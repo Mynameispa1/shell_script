24632eb234b7ebdfb61779b2122b5097db8199c5
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-13 13:31:13 -0600

    complete script

--- 08install_scripts.sh
+++ 08install_scripts.sh
@@ -3,8 +3,16 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
 echo "Error:: Please run script with root user"
+exit 1
 else
 echo "Running with root user"
 fi
 
 yum install mysql -y
+if [ (echo$? -ne 0) ]
+then
+ echo "Error:: Installation is failed"
+else
+ echo "Installation is successful"
+
+
