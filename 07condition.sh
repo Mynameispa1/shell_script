78b1ab7895a165674a9f55a88baa55c07d70e20e
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-14 12:27:15 -0600

    functions added

--- 07condition.sh
+++ 07condition.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 ID=$(id -u)
-R=\e[31m
-G=\e[32m
-Y=\e[33m
-N=\e[34m
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 Log_Path="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
