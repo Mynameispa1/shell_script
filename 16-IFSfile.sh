5d409636669431bc18bf16a52f9e4f0a3c791cfd
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-18 12:33:49 -0600

    Delete old logs

--- 16-IFSfile.sh
+++ 16-IFSfile.sh
@@ -7,7 +7,7 @@ fi
 
 while IFS="     " read -r username password user_id group_id user_fullname
 do 
-    echo "username:$username"
-    echo "passwrod:$password"
-    echo "user_id:$user_id"
-done <<<$file
+    echo "username: $username"
+    echo "passwrod: $password"
+    echo "user_id: $user_id"
+done < $file
