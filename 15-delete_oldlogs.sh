6b6768ae03e59e6c8b5652b9bc772a24769de046
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-18 11:53:17 -0600

    Delete old logs

--- 15-delete_oldlogs.sh
+++ 15-delete_oldlogs.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 source_dir="/tmp/shellscript"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
 
 if [ ! -d $source_dir ]
   then
-    echo "Error:: Given directory is not exist"
+    echo "Error:: $R Given directory is not exist $N"
   else
-    echo "Directory exist"
+    echo "$G Directory exist $N"
 fi
 
 files_to_delete=$(find /tmp/shellscript/ -type f -mtime +14 -name "*.log")
@@ -15,9 +19,9 @@ rm -rf $files_to_delete
 
 if [ $? -ne 0 ]
  then
-   echo "Error:: Old logd are not deleted"
+   echo "Error:: $R Old logd are not deleted $N"
  else
-   echo "Required old logs are deleted"
+   echo "$R Required old logs are deleted $N"
 fi
 
 
