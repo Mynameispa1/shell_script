6c3d3b7701ed2ecae47412468a0d5bea6b7b5dab
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-18 12:35:26 -0600

    Delete old logs

--- 16-IFSfile.sh
+++ 16-IFSfile.sh
@@ -5,7 +5,7 @@ if [ ! -f $file ]
    echo "Error:: The given directory not exist"
 fi
 
-while IFS="     " read -r username password user_id group_id user_fullname
+while IFS=":" read -r username password user_id group_id user_fullname
 do 
     echo "username: $username"
     echo "password: $password"
