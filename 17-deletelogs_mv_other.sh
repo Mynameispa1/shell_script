70f53ff7fc53b9a6f63ad9f271569ece85b2aec0
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-19 12:04:04 -0600

    Delete log and append to other directory

---
+++ 17-deletelogs_mv_other.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source_dir="/tmp/shellscript"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+if [ ! -d $source_dir ]
+  then
+    echo -e "Error:: $R.. Given directory is not exist.. $N"
+  else
+    echo -e "$G Directory exist $N"
+fi
+
+files_to_delete=$(find $source_dir -type f -mtime +14 -name "*.log" -exec mv {} /tmp/pavan/ \; )
+
+rm -rf $files_to_delete
+
+if [ $? -ne 0 ]
+ then
+   echo -e "Error:: $R.. Old logd are not deleted.. $N"
+ else
+   echo -e "$G Deleted $file_to_delete.. $N"
+fi
